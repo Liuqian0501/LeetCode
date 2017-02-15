e3f9a485d694d9a4ea9f17de02e80dc5068f2bc3
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-15 17:24:37 -0600

    Update MaxConsecutiveOnes.java

--- Array/MaxConsecutiveOnes.java
+++ Array/MaxConsecutiveOnes.java
@@ -2,36 +2,35 @@
 public class MaxConsecutiveOnes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+	// TODO Auto-generated method stub
 		int[] nums = {1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,1,1};
-    	//Compare algorithm
-		
+		//Compare algorithm
+
 		//method 1
 		long startTime = System.nanoTime();
-    	int max = 0;
-    	for ( int i=0;i<100;i++) {
-    		max = findMaxConsecutiveOnes(nums);
+		int max = 0;
+		for ( int i=0;i<100;i++) {
+			max = findMaxConsecutiveOnes(nums);
 		}
-    	System.out.println(max);
-    	long endTime = System.nanoTime();
-    	System.out.println("Took "+(endTime - startTime) + " ns"); 
-    	
-    	//method 2
-    	long startTime2 = System.nanoTime();
-    	for ( int i=0;i<100;i++) {
-    		max = findMaxConsecutiveOnes2(nums);
+		System.out.println(max);
+		long endTime = System.nanoTime();
+		System.out.println("Took "+(endTime - startTime) + " ns"); 
+
+		//method 2
+		long startTime2 = System.nanoTime();
+		for ( int i=0;i<100;i++) {
+			max = findMaxConsecutiveOnes2(nums);
 		}
-    	System.out.println(max);
-    	long endTime2 = System.nanoTime();
-    	System.out.println("2 Took "+(endTime2 - startTime2) + " ns"); 
+		System.out.println(max);
+		long endTime2 = System.nanoTime();
+		System.out.println("2 Took "+(endTime2 - startTime2) + " ns"); 
 	}
 
-    public static int findMaxConsecutiveOnes(int[] nums) {
-    	int cons = 0;
-    	int maxCons = 0;
-        for(int i:nums){
-        	if (i==1) {
+	public static int findMaxConsecutiveOnes(int[] nums) {
+		int cons = 0;
+		int maxCons = 0;
+		for(int i:nums){
+			if (i==1) {
 				cons++;
 				//maxCons = maxCons >= cons ? maxCons : cons; //slower
 				if(maxCons<cons){
@@ -40,24 +39,24 @@ public class MaxConsecutiveOnes {
 			}else{
 				cons = 0;
 			}
-        }
-        return maxCons;
-    }
-    public static int findMaxConsecutiveOnes2(int[] nums) {
-    	int cons = 0;
-    	int maxCons = 0;
-        for(int i:nums){
-        	if (i==1) {
+		}
+		return maxCons;
+	}
+	
+    	public static int findMaxConsecutiveOnes2(int[] nums) {
+		int cons = 0;
+		int maxCons = 0;
+		for(int i:nums){
+			if (i==1) {
 				cons++;
 
 				if(maxCons<cons){
 					maxCons = cons;
 				}
-
 			}else{
 				cons = 0;
 			}
-        }
-        return maxCons;
-    }
+        	}
+        	return maxCons;
+    	}
 }
