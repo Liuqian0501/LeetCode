2eac083ada4d1e6a47d24dad8c660202b7f3e5a9
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-15 20:24:29 -0600

    Update ThreeSum.java

--- Array/ThreeSum.java
+++ Array/ThreeSum.java
@@ -8,29 +8,29 @@ public class ThreeSum {
 		
     public static List<List<Integer>> threeSum(int[] nums) {
         
-    List<List<Integer>> res = new ArrayList<>();
-    Arrays.sort(nums);
-    for (int i = 0; i + 2 < nums.length && nums[i] <= 0; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
-            continue;
-        }
-        int j = i + 1, k = nums.length - 1;  
-        int target = -nums[i];
-        while (j < k){
-            if (nums[j] + nums[k] == target) {
-                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                j++;
-                k--;
-                while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
-                while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
-            } else if (nums[j] + nums[k] > target) {
-                k--;
-            } else {
-                j++;
-            }
-        }
-    }
-    return res;
+	List<List<Integer>> res = new ArrayList<>();
+	Arrays.sort(nums);
+	for (int i = 0; i + 2 < nums.length && nums[i] <= 0; i++) {
+		if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+		    continue;
+		}
+		int j = i + 1, k = nums.length - 1;  
+		int target = -nums[i];
+		while (j < k){
+		    if (nums[j] + nums[k] == target) {
+			res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+			j++;
+			k--;
+			while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
+			while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
+		    } else if (nums[j] + nums[k] > target) {
+			k--;
+		    } else {
+			j++;
+		    }
+		}
+	}
+    	return res;
         
     }
     public static void main(String args[]) {
@@ -40,7 +40,6 @@ public class ThreeSum {
         List<List<Integer>> a = threeSum(nums);
         long endTime = System.nanoTime();
         System.out.println("Took "+(endTime - startTime)/1000000 + " ms");
-        //System.out.println(a.toString());
     }
 
 }
