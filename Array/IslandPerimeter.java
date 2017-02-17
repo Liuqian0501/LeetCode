dd3aca1cce9e30d62f9ad0dc57b82180f93ed8b5
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-16 19:02:00 -0600

    Update IslandPerimeter.java

--- Array/IslandPerimeter.java
+++ Array/IslandPerimeter.java
@@ -6,17 +6,17 @@
 public class IslandPerimeter {
     public static int islandPerimeter(int[][] grid) {
     	int width = grid.length, length = grid[0].length;
-    	int neighbours=0 , island = 0;
+    	int result = 0;
     	for(int i=0;i<width;i++){
     		for (int j = 0; j < length; j++) {
 				if (grid[i][j]==1) {
-					island++;
-					if (j<length-1&&grid[i][j+1]==1)neighbours++;
-					if (i<width-1 && grid[i+1][j]==1)neighbours++;
+					result+=4;
+					if (j<length-1 && grid[i][j+1]==1)result-=2;
+					if (i<width-1 && grid[i+1][j]==1)result-=2;
 				}
 			}
     	}
-        return 4*island - 2*neighbours;
+        return result;
     }
 	public static void main(String args[]) {
 		int[][] str = {{0,1,0,0,0},{1,1,1,0,0},{0,1,0,0,0},{1,1,0,0,0}};
