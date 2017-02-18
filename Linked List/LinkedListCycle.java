5e0eefb389e01af48de1906b5c6684904ceb7f36
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-18 14:02:20 -0600

    Create LinkedListCycle.java

---
+++ Linked List/LinkedListCycle.java
@@ -0,0 +1,30 @@
+public class LinkedListCycle {
+
+	public static boolean hasCycle(ListNode head) {
+        if(head == null) return false;
+    	ListNode walk = head;
+    	ListNode run = head;
+    	while(run.next != null && run.next.next != null){
+    		run = run.next.next;
+    		walk = walk.next;
+    		if(walk==run){
+    			return true;
+    		}
+    	}
+        return false;       
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ListNode aListNode = new ListNode(1);
+		ListNode bListNode = new ListNode(2);
+		ListNode cListNode = new ListNode(3);
+		ListNode dListNode = new ListNode(4);
+		aListNode.next = bListNode;
+		bListNode.next = cListNode;
+		cListNode.next = dListNode;
+		dListNode.next = aListNode;
+		System.out.println(hasCycle(aListNode));
+
+	}
+
+}
