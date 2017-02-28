6c2a8c4e7ab3761711a1700394f8309b7cedf432
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-28 14:38:55 -0600

    Update ReverseBits.java

--- Bit Manipulation/ReverseBits.java
+++ Bit Manipulation/ReverseBits.java
@@ -2,11 +2,11 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int res = n&1;
-        n >>= 1;
+        n >>>= 1;
         for(int i = 1; i < 32; i++){
-            res <<= 1;
+            res = res *2;
             res += n&1;
-            n >>= 1;
+            n >>>= 1;
         }
         return res;
     }
