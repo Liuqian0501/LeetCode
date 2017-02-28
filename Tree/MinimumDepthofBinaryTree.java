b8722f6bf0cf7566508d82fc50da37b353b97599
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-27 18:50:42 -0600

    Update MinimumDepthofBinaryTree.java

--- Tree/MinimumDepthofBinaryTree.java
+++ Tree/MinimumDepthofBinaryTree.java
@@ -22,3 +22,16 @@ public class Solution {
         if(root.left == null && root.right == null) min = min < depth ? min : depth; 
     }
 }
+
+
+public class Solution {
+
+    public int minDepth(TreeNode root) {
+      if(root == null) return 0;
+      int left = minDepth(root.left);
+      int right = minDepth(root.right);
+      return (left == 0 || right == 0) ? left + right + 1 : (left < right ?  left + 1 : right + 1);
+    }
+    
+
+}
