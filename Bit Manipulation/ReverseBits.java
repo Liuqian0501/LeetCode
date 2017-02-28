ef9eea924801813ff4a564a6029f2a3334d875a4
Liuqian0501 <liu.qian.am@gmail.com>
2017-02-28 14:37:49 -0600

    Create ReverseBits.java

---
+++ Bit Manipulation/ReverseBits.java
@@ -0,0 +1,27 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = n&1;
+        n >>= 1;
+        for(int i = 1; i < 32; i++){
+            res <<= 1;
+            res += n&1;
+            n >>= 1;
+        }
+        return res;
+    }
+}
+
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int res = n&1;
+        n = n >>> 1;
+        for(int i = 1; i < 32; i++){
+            res = res *2;
+            res += n&1;
+            n = n >>>1;
+        }
+        return res;
+    }
+}
