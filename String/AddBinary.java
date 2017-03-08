88b8a7eb64e868f65fbb92508bc5823d07924c31
Liuqian0501 <liu.qian.am@gmail.com>
2017-03-07 19:27:00 -0600

    Update AddBinary.java

--- String/AddBinary.java
+++ String/AddBinary.java
@@ -1,21 +1,22 @@
 public class Solution {
     public String addBinary(String a, String b) {
-        // To solve binary adding problem, we need to use a carry 
+        // To solve binary adding problem, we need to use a carry
+        // we create a new string as result
         char carry = '0';
         String s = "";
         int lenA = a.length();
         int lenB = b.length();
         if(lenA > lenB){
-            b = addZeros(b, lenA - lenB, ""); // add zeros to s + b
+            b = addZeros(b, lenA - lenB); // add zeros to s + b
         }else{
-            a = addZeros(a, lenB - lenA, ""); // add zeros to s + a
+            a = addZeros(a, lenB - lenA); // add zeros to s + a
         }
         int len = a.length() - 1;
         
         while(len >= 0){
             if(a.charAt(len) == b.charAt(len)){
                 s = carry + s;
-                carry = (a.charAt(len) == '0') ? '0' : '1';
+                carry = a.charAt(len);//carry = (a.charAt(len) == '0') ? '0' : '1';
             }else{
                 s = ((carry == '1') ? '0' : '1') + s;
                 // carry still the same;
@@ -25,11 +26,11 @@ public class Solution {
 
         return (carry == '1')? '1' + s : s;
     }
-    private String addZeros(String t, int n, String s){
+    private String addZeros(String t, int n){
+        String s = "";
         for(int i = 0; i < n; i++ ){
-            s = s + "0";
+            s = s + '0';
         }
-        t = s + t;
-        return t;
+        return s + t;
     }
 }
