75618922d469569e85a12df9e733315affeafc89
Liuqian0501 <liu.qian.am@gmail.com>
2017-03-09 13:12:28 -0600

    Update ValidAnagram.java

--- Array/ValidAnagram.java
+++ Array/ValidAnagram.java
@@ -32,3 +32,25 @@ public class Solution {
         return s.equals(t);
     }
 }
+
+
+//solution 3
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        //we can use prime Map (for group)
+        int[] primeMap = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 41, 43, 47, 53,
+                            59, 61, 67, 71, 73, 79, 83, 89, 97, 101,103};//maximum 10609 z;
+        int slen = s.length();
+        int tlen = t.length();
+        System.out.println(slen);
+        if(slen != tlen) return false;
+        int k1 = 1 , k2 = 1;
+        for(int i = 0; i < slen; i++){
+            k1 *= primeMap[s.charAt(i) - 'a'];
+            k2 *= primeMap[t.charAt(i) - 'a'];
+        }
+        
+        return (k1 == k2) ? true : false;
+    }
+}
