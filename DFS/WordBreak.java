0300e50c12bce06a74103bb9e26e1bcd86ffd213
Liuqian0501 <liu.qian.am@gmail.com>
2017-03-13 14:14:34 -0500

    Update WordBreak.java

--- DFS/WordBreak.java
+++ DFS/WordBreak.java
@@ -1,39 +1,31 @@
 public class Solution {
     boolean res;
+    String str;
     public boolean wordBreak(String s, List<String> wordDict) {
         //I am going to solve this problem using BFS
         //base case no s, no wordDict, s.length = 0
         //Case 1: no sub word in dictionary;
-        //Case 2: one of them in dictionary; resursively call breakHelper(s.substring(0,i+1)+marker+ s.substring(i+1), wordDict) for all word in wordDictionary
+        //Case 2: one of them in dictionary; resursively call breakHelper(start, wordDict) for all word in wordDictionary
         
         // if new string.length = 0, return true
         if(s == null || s.length() == 0 || wordDict == null ||wordDict.size() == 0) return false;
         res = false;
-        breakHelper(s, wordDict, s.length());
+        int start = 0;
+        str = s;
+        breakHelper(wordDict, start);
         return res;
     }
-    private void breakHelper(String s, List<String> wordDict, int remain){
-        if (remain == 0) {
+    private void breakHelper(List<String> wordDict, int start){
+        if(res) return;
+        if (str.length() == start) {
             res = true;
             return;
         }
-        if(res) return;
-        for(String word : wordDict){
-            int len = word.length();
-            for(int i = 0; i + len - 1 < s.length(); i++){
-                if(word.equals(s.substring(i,i+len))){//s.substring(i,i+len+1) equals to word
-                    String newS = s.substring(0,i) + marker(len) + s.substring(i+len);
-                    breakHelper(newS, wordDict, remain - len);
-                }
+        for(int i = start + 1; i <= str.length(); i++){
+            if(wordDict.contains(str.substring(start,i))){//s.substring(i,i+len+1) equals to word
+                breakHelper(wordDict, i);
             }
         }
-    }
-    private String marker(int len){
-        String s = "";
-        for(int i = len; i > 0; i--){
-            s = s + '&'; 
-        }
-        return s;
     }
 }
 
