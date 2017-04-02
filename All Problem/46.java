f12857a789193e2b2f50d8ff4ffe445d1001e43e
Liuqian0501 <liu.qian.am@gmail.com>
2017-04-02 14:26:37 -0500

    Create 46.java

---
+++ All Problem/46.java
@@ -0,0 +1,25 @@
+public class Solution {
+    //The basic idea is to check every value at each position, if index i can have value n, then remove n from the choice set and solve the subproblem using the same algothm. Complete choice property.
+    public List<List<Integer>> permute(int[] nums) {
+        if(nums == null || nums.length == 0) return null;
+        boolean[] choice = new boolean[nums.length];//define a choice set
+        List<List<Integer>> list= new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        helper(nums.length, nums, choice, list, list2);
+        return list;
+    }
+    private void helper(int len, int[] nums,boolean[] choice, List<List<Integer>> list, List<Integer> list2){
+        if(list2.size() == len){
+            list.add(list2);
+            return;
+        }
+        for(int i = 0; i< len; i++){
+            if(choice[i]) continue;
+            choice[i] = true;
+            List<Integer> newList = new ArrayList<>(list2);
+            newList.add(nums[i]);
+            helper(len, nums, choice, list, newList);
+            choice[i] = false;
+        }
+    }
+}
