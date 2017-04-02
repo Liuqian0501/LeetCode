0342492848df1fb0a2028e0e26cff0de29874488
Liuqian0501 <liu.qian.am@gmail.com>
2017-04-02 14:34:46 -0500

    Create 294.java

---
+++ All Problem/294.java
@@ -0,0 +1,33 @@
+https://leetcode.com/problems/flip-game-ii/#/description
+public class Solution {
+    private static Set<String> winSet = new HashSet<>();
+    private static Set<String> lossSet = new HashSet<>();
+    public boolean canWin(String s) {
+        if(s == null || s.length() < 2){
+            return false;
+        }
+        if(lossSet.contains(s)){
+            return false;
+        }
+        if(winSet.contains(s)){
+            return true;
+        }
+        String t;
+        for(int i = 0; i< s.length() - 1; i++){
+            if(s.startsWith("++", i)){
+                t = s.substring(0, i) + "--" + s.substring(i + 2);
+
+                if(!canWin(t)){
+                    lossSet.add(t);
+                    winSet.add(s);
+                    return true;
+                }else{
+                    winSet.add(t);
+                }
+            }
+        }
+
+        lossSet.add(s);
+        return false;
+    }
+}
