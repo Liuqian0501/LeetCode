d9e65a720ee73c70307b062be321ed2d814d9f92
Liuqian0501 <liu.qian.am@gmail.com>
2017-04-03 16:25:02 -0500

    Create 367.java

---
+++ All Problem/367.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/valid-perfect-square/#/description
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num < 0){
+            return false;
+        }
+        long head = 0, tail = num;
+        long mid = head + (tail - head)/2, midsqr = mid*mid;
+        while(head <= tail){
+            if(midsqr < num){
+                head = mid + 1;
+            }else if(midsqr > num){
+                tail = mid - 1;
+            }else{
+                return true;
+            }
+            mid = head + (tail - head)/2;
+            midsqr = mid*mid;
+        }
+        return false;
+    }
+}
