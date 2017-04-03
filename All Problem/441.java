75afb111b8848f587b704e26b21005f77095cde1
Liuqian0501 <liu.qian.am@gmail.com>
2017-04-03 14:57:11 -0500

    Update 441.java

--- All Problem/441.java
+++ All Problem/441.java
@@ -1,11 +1,14 @@
 https://leetcode.com/problems/arranging-coins/
+
+//1+2+3+...+x <= n
+// x(x+1)/2 <= n
 public class Solution {
     public int arrangeCoins(int n) {
         if(n<=0){
             return 0;
         }
         long start = 1, end = (long) n;
-        long mid = start + (end - start)/2;
+        long mid = start + (end - start)/2;//通过令mid = start + (end - start)/2; 可以加快计算速度。
         while(start <= end){
             mid = start + (end - start)/2;
             if(mid*mid*0.5 + mid*0.5 < (long) n){
@@ -17,6 +20,7 @@ public class Solution {
             }
             //cal = mid*mid/2 + mid/2;
         }
-        return (int) start -1;
+        //当执行完整个循环后，start-1 = end，且start对应的值比目标值大，end对应的值比目标值小。
+        return (int) end;
     }
 }
