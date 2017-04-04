e22f9a682c507ccd52dda6ecfb3d1c15f0ccf307
Liuqian0501 <liu.qian.am@gmail.com>
2017-04-03 21:48:28 -0500

    Update 261.java

--- All Problem/261.java
+++ All Problem/261.java
@@ -2,20 +2,20 @@
 public class Solution {
     public boolean validTree(int n, int[][] edges) {
         // initialize n isolated islands
-        int[] nums = new int[n];
-        Arrays.fill(nums, -1);
+        int[] parent = new int[n];
+        Arrays.fill(parent, -1);
         
         // perform union find
         for (int i = 0; i < edges.length; i++) {
-            int x = find(nums, edges[i][0]);
-            int y = find(nums, edges[i][1]);
+            int x = find(parent, edges[i][0]);
+            int y = find(parent, edges[i][1]);
             
             // if two vertices happen to be in the same set
             // then there's a cycle
             if (x == y) return false;
             
             // union
-            nums[x] = y;
+            parent[x] = y;//y is our new root
         }
         
         return edges.length == n - 1;//for a valid tree with n node, there must be n - 1 edges
